5959152ccdcb8f49c62bff57057bacec8abb91c7
KiralyAlparCsaba <kiraly.alpar.csaba@student.ms.sapientia.ro>
2023-03-22 14:04:54 +0200

    Labor4&Labor5

--- labor-04/src/lab4_2/Main.java
+++ labor-04/src/lab4_2/Main.java
@@ -7,38 +7,57 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import static lab4_1.Main.readFromCSVFile;
+
 public class Main {
 
     public static void main(String[] args) {
-        public static ArrayList<Customer> readFromCSVFile (String fileName){
+        ArrayList<Customer> customers = readFromCSVFile("lab4_2_input.csv");
+        for (Customer customer : customers) {
+            System.out.println(customer);
+        }
+    }
+
+
+        public static ArrayList<Customer> readFromCSVFile(String fileName){
             ArrayList<Customer> customers = new ArrayList<>();
             try (Scanner scanner = new Scanner(new File(fileName))) {
+                int i =0;
                 while (scanner.hasNextLine()) {
                     String line = scanner.nextLine();
                     if (line.isEmpty()) {
                         continue;
                     }
                     String[] items = line.split(",");
-                    String type = items[0].trim();
-                    if (type.equals("Customer")) {
+
+                    while (items[0].trim().equals("Customer")) {
                         String firstName = items[1].trim();
                         String lastName = items[2].trim();
                         customers.add(new Customer(firstName, lastName));
-                    } else if (type.equals("Account")) {
-                        Customer customer = customers.get(customers.size() - 1);
+                        Customer currentCustomer = customers.get(i);
+                        i++;
+
+                    if (scanner.hasNextLine()) {
+                        line = scanner.nextLine();
+                        items = line.split(",");
+                    } else break;
+                    while (items[0].trim().equals("Account")) {
                         String accountNr = items[1].trim();
                         double balance = Double.parseDouble((items[2].trim()));
-                        BankAccount account = new BankAccount(accountNr);
-                        account.deposit(balance);
-                        customer.addAccounts(account);
+                        currentCustomer.addAccounts(new BankAccount(accountNr,balance));
+                        if(scanner.hasNextLine()) {
+                            line = scanner.nextLine();
+                            items = line.split(",");
+                        }else break;
+                        }
                     }
+
 
                 }
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
             }
-            //return customers;
-        }
+            return customers;
     }
 }
 
